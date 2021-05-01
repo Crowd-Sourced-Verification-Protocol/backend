3339e835e17c7188d2dd6b18093c37d443d84509
Nandit <nanditmehra123@yahoo.com>
2021-05-01 12:10:40 +0530

    getRating endpoint

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/addReport', (req, res) => {
       let reportDocument = {
         userId: req.body.userId,
         url: req.body.url,
-        reportReason: req.body.reportReason, 
+        reportReason: req.body.reportReason,
         reasonType: req.body.reasonType, // 1-5 for checkbox reason, 6 for manually written by the user
         type: "report"
       };
@@ -91,6 +91,56 @@ app.post('/addReport', (req, res) => {
   }
   run();
   res.send('done')
+})
+
+
+app.get('/getRating', (req, res) => {
+  console.log('entered getRating with parameters: ', req.body);
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  async function run() {
+    try {
+      console.log('entered try block');
+      await client.connect();
+      console.log('connected correctly to the server');
+
+      const db = client.db("db1");
+
+      const col = db.collection("cl1");
+
+      const query = { url: req.body.url };
+
+      const options = {
+        projection: { rating: 1 }
+      }
+
+      const cursor = await col.find(query, options);
+
+      // print a message if no documents were found!
+      if ((await cursor.count) === 0) {
+        console.log('no documents found!');
+        res.send({});
+      }
+
+      const ratingsArray = [];
+      await cursor.forEach(function (item) {
+        if (item.rating)
+          ratingsArray.push(item.rating);
+      });
+
+      const totalRatings = ratingsArray.length;
+      const averageRating = (ratingsArray.reduce((a, b) => a + b, 0)) / totalRatings;
+      res.send({
+        totalRatings: totalRatings,
+        averageRating: averageRating
+      })
+    } catch (e) {
+      console.log('got an error:', e);
+      res.send('error:', e)
+    } finally {
+      await client.close();
+    }
+  }
+  run();
 })
 
 console.log('restfulAPI started at port', port);
